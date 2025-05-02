ab99ad19f966267a43d6898543cada7b40753616
Cyn <amandaodoo@gmail.com>
2025-05-01 21:11:28 -0300

    borrar metadata widgets

--- 014 Laboratorio Mejora de Color-Segmentacion/script.py
+++ 014 Laboratorio Mejora de Color-Segmentacion/script.py
@@ -1,17 +1,16 @@
 import nbformat
 
-# Rutas corregidas
 ruta_entrada = r"C:\Users\misse\Desktop\github\14_LAB_Integrador_2.ipynb"
-ruta_salida = r"C:\Users\misse\Desktop\github\14_LAB_Integrador_2_fixed.ipynb"
+ruta_salida = r"C:\Users\misse\Desktop\github\14_LAB_Integrador_2_clean.ipynb"
 
 # Leer el notebook
 with open(ruta_entrada, encoding="utf-8") as f:
     nb = nbformat.read(f, as_version=4)
 
-# Reparar metadata si es necesario
-if "widgets" in nb["metadata"] and "state" not in nb["metadata"]["widgets"]:
-    nb["metadata"]["widgets"]["state"] = {}
+# Eliminar por completo metadata.widgets
+if "widgets" in nb["metadata"]:
+    del nb["metadata"]["widgets"]
 
-# Guardar el notebook corregido
+# Guardar el notebook limpio
 with open(ruta_salida, "w", encoding="utf-8") as f:
     nbformat.write(nb, f)
